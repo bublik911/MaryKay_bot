121efb246eb95b6ab5e30be91e07c17f3192db29
bublik911 <110043747+bublik911@users.noreply.github.com>
2023-03-23 00:25:36 +0300

    Проверка на роль

---
+++ keyboards/client_or_consultant.py
@@ -0,0 +1,9 @@
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import ReplyKeyboardMarkup
+
+
+def client_or_consultant() -> ReplyKeyboardMarkup:
+    keyboard = ReplyKeyboardBuilder()
+    keyboard.button(text="Клиент")
+    keyboard.button(text="Консультант")
+    return keyboard.as_markup(one_time_keyboard=True, resize_keyboard=True)
