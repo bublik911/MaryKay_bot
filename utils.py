6b136a22bd07287a2b0548e53eefc0666b6f7f3d
bublik911 <110043747+bublik911@users.noreply.github.com>
2023-03-27 10:34:53 +0300

    Добавлена функция формирования списка клиентов

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+from DataBase.models_db import *
+from aiogram.types import Message
 months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
 
 
@@ -20,3 +22,16 @@ def month_to_date(month) -> int or bool:
         else:
             return months.index(month) + 1
     return False
+
+
+def create_clients_list(message: Message) -> list:
+    pid = Consultant.get(Consultant.chat_id == message.chat.id).id
+    clients = Client.select().where(Client.pid == pid & Client.deleted_at.is_null())
+    response = []
+    k = 0
+    for client in clients:
+        k += 1
+        month, day = str(client.date).split("-")[1:]
+        response.append(f"{k} {client.name} {month}-{day}\n"
+                        f" +7{client.phone}")
+    return response
