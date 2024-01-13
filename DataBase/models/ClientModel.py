3803c9e6ad76dc348ed48d1763cdc7aa8ba3e268
Egor <ekurlenko17@gmail.com>
2024-01-13 23:07:53 +0300

    db refactoring

---
+++ DataBase/models/ClientModel.py
@@ -0,0 +1,17 @@
+from peewee import *
+
+from DataBase.models.ConsultantModel import Consultant
+from DataBase.config import db
+
+
+class Client(Model):
+    pid = ForeignKeyField(Consultant)
+    name = CharField()
+    phone = CharField()
+    chat_id = IntegerField(null=True)
+    date = DateField()
+    deleted_at = DateField(null=True)
+
+    class Meta:
+        db_table = 'client'
+        database = db
