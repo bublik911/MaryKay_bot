73996885ef233b9f82dde7f9578d00a843e63d1f
Egor <ekurlenko17@gmail.com>
2024-01-30 22:39:34 +0300

    routing

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,14 +1,46 @@
+import handlers
+
 from aiogram import Router
+from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command
 from aiogram.types import Message
-from DataBase.config import db
+
+from states import Menu, CheckBase, AddClient, Sending
+
 from keyboards.main_menu_keyboard import main_menu_keyboard
-# from handlers import add_client, check_clients
+
+from misc.consts import CHECK_CLIENTS_BASE, ADD_CLIENT, SENDING
+
+
 router = Router()
 
 
-@router.message(Command("menu"))
-async def main_menu(message: Message):
+@router.message(
+    Command("menu")
+)
+@router.message(
+    Menu.transition
+)
+async def main_menu(message: Message, state: FSMContext):
+    await state.clear()
     await message.answer("Что вы хотите сделать?",
                          reply_markup=main_menu_keyboard())
-    # router.include_routers(check_clients.router, add_client.router)
+    await state.set_state(Menu.waiting)
+
+
+@router.message(
+    Menu.waiting
+)
+async def answer_routing(message: Message, state: FSMContext):
+
+    if message.text == CHECK_CLIENTS_BASE:
+        await state.set_state(CheckBase.transition)
+        await handlers.check_clients.check_base(message, state)
+
+    elif message.text == ADD_CLIENT:
+        await state.set_state(AddClient.transition)
+        await handlers.add_client.start(message, state)
+
+    elif message.text == SENDING:
+        await state.set_state(Sending.transition)
+        await handlers.sending.sending_start(message, state)
