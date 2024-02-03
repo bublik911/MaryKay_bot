fc330f720ac1a62c0b4837098e71808c7f45f26b
Egor <ekurlenko17@gmail.com>
2024-02-03 17:10:51 +0300

    изменить на "Всё верно"

--- handlers/add_client.py
+++ handlers/add_client.py
@@ -14,7 +14,7 @@ from states import AddClient
 from DataBase.repositories import ClientRepository, ConsultantRepository
 
 from misc.utils import phone_parse, month_to_date, date_to_month, correct_date
-from misc.consts import ADD_CLIENT, YES_WITH_MARK, FILL_AGAIN
+from misc.consts import ADD_CLIENT, ALL_OK_WITH_MARK, FILL_AGAIN
 
 router = Router()
 
@@ -73,7 +73,7 @@ async def add_client_day(message: Message, state: FSMContext):
 
 @router.message(
     AddClient.commit,
-    Text(YES_WITH_MARK)
+    Text(ALL_OK_WITH_MARK)
 )
 async def commit(message: Message, state: FSMContext):
     pid = ConsultantRepository.get_consultant_id_by_chat_id(message.chat.id)
--- keyboards/check_client_keyboard.py
+++ keyboards/check_client_keyboard.py
@@ -1,12 +1,12 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.types import ReplyKeyboardMarkup
 
-from misc.consts import YES_WITH_MARK, FILL_AGAIN
+from misc.consts import ALL_OK_WITH_MARK, FILL_AGAIN
 
 
 def check_client_keyboard() -> ReplyKeyboardMarkup:
     keyboard = ReplyKeyboardBuilder()
-    keyboard.button(text=YES_WITH_MARK)
+    keyboard.button(text=ALL_OK_WITH_MARK)
     keyboard.button(text=FILL_AGAIN)
     return keyboard.as_markup(resize_keyboard=True,
                               one_time_keyboard=True)
